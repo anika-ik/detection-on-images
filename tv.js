86abfff39a3e000b0732ebadb55802c8629b7d28
anika-ik <79695696+anika-ik@users.noreply.github.com>
2021-11-29 15:46:33 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,25 @@
+status="";
+
+function preload(){
+    img= loadImage('tv.jpg')
+}
+
+function setup(){
+    canvas= createCanvas(380,380);
+    canvas.center();
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status : Detecting objects";
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+    status= true;
+    
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+}
